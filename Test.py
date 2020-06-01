af8c004a239bb2bf18a94a5c8dcc3d0bce6d7918
Karthikeya Vishwa . N <63010942+Karthik360-tech@users.noreply.github.com>
2020-06-01 16:47:58 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,10 @@
+#Sample Input :
+ 6
+ 1 2 3 4 10 11
+#Sample Output : 31
+#Explanation : 1+2+3+4+10+11 = 31 
+
+#We print the sum of the array's elements: .
 ar_count = int(input())
 ar = list(map(int ,input().split()))
 a=0
